64df5f75abe52cbfcd33a2a5c6252b9da6d86012
Johannes Krabbe <Johannes@Krabbe.net>
2022-11-03 12:55:33 +0100

    disable database for deployment testing

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { AppDataSource } from "./data-source";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-AppDataSource.initialize()
-    .then(async () => {
-        const app = require("./app").default;
+// AppDataSource.initialize()
+//    .then(async () => {
+const app = require("./app").default;
 
-        const PORT = process.env.PORT || 3001;
-        app.listen(PORT, () => {
-            console.info(`Running on port ${PORT}`);
-        });
-        return app;
-    })
-    .catch((error) => console.log(error));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.info(`Running on port ${PORT}`);
+});
+//         return app;
+//     })
+//     .catch((error) => console.log(error));
